f234efbc414ef1819a675f1905cb240267c9a45b
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-05-04 20:16:55 -0400

    updated routes

--- models/profanity.js
+++ models/profanity.js
@@ -4,3 +4,5 @@ const filter = new Filter() //
 const cleanInput = filter.clean(userInput)
 
 const filterEx = new Filter({ exclude: ['foo', 'bar'] }); // add new words to the bad list
+
+console.log(filter.clean("don't be an asshole"))
