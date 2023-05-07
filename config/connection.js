b4bf458b7371d84060d86600ba3503d0f331b851
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-05-07 19:50:32 -0400

    fixed sequelize error

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
 if (process.env.HEALTHHIVE_URL) {
-    sequelize = new Sequelize(process.env.HEALTHHIVE_URL);
+  sequelize = new Sequelize(process.env.HEALTHHIVE_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
