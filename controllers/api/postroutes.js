70857ad0a1b2091e63c216a670d8d6a29f39aced
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-05-09 12:22:59 -0400

    fixed sintax with api

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
-const {User,Post,Comment} = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 const Filter = require("bad-words")
-  filter = new Filter()
+filter = new Filter()
 
 
 // Get all posts
 router.get("/", (req, res) => {
     Post.findAll({
-            attributes: ["id", "content", "title", "created_at"],
-            order: [
-                ["created_at", "DESC"]
-            ],
-            include: [{
-                    model: User,
-                    attributes: ["username"],
-                },
-                {
-                    model: Comment,
-                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-                    include: {
-                        model: User,
-                        attributes: ["username"],
-                    },
-                },
-            ],
-        })
+        attributes: ['id', 'title', 'post_content', 'user_id'],
+        order: [
+            ["created_at", "DESC"]
+        ],
+        include: [{
+            model: User,
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id"],
+            include: {
+                model: User,
+                attributes: ["username"],
+            },
+        },
+        ],
+    })
         .then((dbPostData) => res.json(dbPostData))
         .catch((err) => {
             console.log(err);
@@ -36,24 +36,24 @@ router.get("/", (req, res) => {
 // Get a single post
 router.get("/:id", (req, res) => {
     Post.findOne({
-            where: {
-                id: req.params.id,
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'title', 'post_content', 'user_id'],
+        include: [{
+            model: User,
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id"],
+            include: {
+                model: User,
+                attributes: ["username"],
             },
-            attributes: ["id", "content", "title", "created_at"],
-            include: [{
-                    model: User,
-                    attributes: ["username"],
-                },
-                {
-                    model: Comment,
-                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-                    include: {
-                        model: User,
-                        attributes: ["username"],
-                    },
-                },
-            ],
-        })
+        },
+        ],
+    })
         .then((dbPostData) => {
             if (!dbPostData) {
                 res.status(404).json({
@@ -73,10 +73,10 @@ router.get("/:id", (req, res) => {
 router.post("/", withAuth, (req, res) => {
     console.log("creating");
     Post.create({
-            title: filter.clean(req.body.title),
-            content: filter.clean(req.body.post_content),
-            user_id: req.session.user_id
-        })
+        title: filter.clean(req.body.title),
+        content: filter.clean(req.body.post_content),
+        user_id: req.session.user_id
+    })
         .then((dbPostData) => res.json(dbPostData))
         .catch((err) => {
             console.log(err);
@@ -87,13 +87,13 @@ router.post("/", withAuth, (req, res) => {
 // Update a post
 router.put("/:id", withAuth, (req, res) => {
     Post.update({
-            title: filter.clean(req.body.title),
-            content: filter.clean(req.body.post_content),
-        }, {
-            where: {
-                id: req.params.id,
-            },
-        })
+        title: filter.clean(req.body.title),
+        content: filter.clean(req.body.post_content),
+    }, {
+        where: {
+            id: req.params.id,
+        },
+    })
         .then((dbPostData) => {
             if (!dbPostData) {
                 res.status(404).json({
@@ -112,10 +112,10 @@ router.put("/:id", withAuth, (req, res) => {
 //Delete a post
 router.delete("/:id", withAuth, (req, res) => {
     Post.destroy({
-            where: {
-                id: req.params.id,
-            },
-        })
+        where: {
+            id: req.params.id,
+        },
+    })
         .then((dbPostData) => {
             if (!dbPostData) {
                 res.status(404).json({
--- public/js/logout.js
+++ public/js/logout.js
@@ -7,7 +7,7 @@ async function logout() {
     });
 
     if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/login');
     } else {
         alert(response.statusText);
     }
