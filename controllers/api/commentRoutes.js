f8a4461d1a0e132f9c8386c89011dff595432a7e
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-05-08 20:17:41 -0400

    route changes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,36 +1,36 @@
 const router = require('express').Router();
-
-const { Comment } = require('../../models');
+const {Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
-
 const Filter = require('bad-words')
   filter = new Filter()
-
-//http://localhost:3001/api/comments/
-router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  
+  
+  //Get all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
-
+  
+  //Create a comment
 router.post('/', withAuth, (req, res) => {
-  if (req.session) {
-    Comment.create({
-      comment_text: filter.clean(req.body.comment_text),
-      user_id: req.session.user_id,
-      post_id: req.body.post_id
-    })
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  }
-});
-
-
-
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+      }
+  });
+  
+  
 module.exports = router;
+
