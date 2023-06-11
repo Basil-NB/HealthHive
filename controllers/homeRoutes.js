08bfaaebfbb7abe6651b56f3f9ea1a0b186c3bce
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-06-11 19:46:44 -0400

    welcome bug fix

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,42 +1,40 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+const withAuth = require("../utils/auth")
 
 
-router.get('/', (req, res) => {
-    if (!req.session.loggedIn) {
-        return res.redirect('/welcome')
-    }
-    Post.findAll({
-        attributes: ['id', 'title', 'post_content', 'user_id'],
-        include: [{
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
-            include: {
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: ['id', 'title', 'post_content', 'user_id'],
+            include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                model: User,
+                attributes: ['username']
+                }
+            },
+            {
                 model: User,
                 attributes: ['username']
             }
-        },
-        {
-            model: User,
-            attributes: ['username']
-        }
-        ]
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
-
-            res.render('blog', {
-                posts,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+            ]
         });
+    
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+    
+        res.render('blog', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
 });
+
 
 router.get('/post/:id', (req, res) => {
     Post.findOne({
